4fc4b87b71617e14e726b0d7a13caa2a977529c7
Anderson-Silva1 <notebookanderson1@gmail.com>
2024-07-10 15:08:41 -0300

    Aula 1, 2, 3 e 4... porém estou fazendo a aula 4 ela vai vir completa no próximo commit

---
+++ 1-introducao/3.1-exemplo-jsx.js
@@ -0,0 +1,15 @@
+
+function App() {
+    const mesage = "Hello World!"
+    const text = "My first App React"
+    return (
+        <>
+            <h1>{mesage}</h1>
+            <p>{text}</p>
+        </>
+)
+}
+
+// links especiais:
+// https://via.placeholder.com/500 => Este link vai criar uma imagem
+
